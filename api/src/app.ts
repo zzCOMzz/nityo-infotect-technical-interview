1c25b995251c6f9b81de91f64f9c5c903f4dad3c
zzcomzz <irvanrefnaldy98@gmail.com>
2025-11-10 22:27:39 +0700

    <feat> : :sparkles: adding cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
 import { errorHandler } from "./middleware/errorHandlers";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(errorHandler);
 
 app.use("/api", routes);
